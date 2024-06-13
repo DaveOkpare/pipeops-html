86d84bb818283693bfc8b6a0de7c46e4d29608f7
DaveOkpare <okparedave@gmail.com>
2024-06-13 15:18:05 +0100

    added dummy endpoint

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from typing import Union
+from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse
 
-from fastapi import FastAPI
+from whatsapp.utils import verify_webhook
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def health():
+    return {"message": "Health check"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/webhook")
+def verification(request: Request):
+    response = verify_webhook(request)
+    return PlainTextResponse(response)
+
+
+@app.post("/webhook")
+def notification():
+    pass
